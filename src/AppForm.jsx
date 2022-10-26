cacbc2ad4fe285c093b5f0a51ade76d38f54d180
gomgom35b <gongwon35.b@gmail.com>
2022-10-26 13:49:38 +0900

    form

---
+++ src/AppForm.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useState } from "react";
+
+export default function AppForm() {
+  const [form, setFrom] = useState({
+    name: "",
+    email: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  // const handleChange = (e) => {
+  //   setFrom({
+  //     ...form,
+  //     [e.target.name]: e.target.value,
+  //   });
+  // };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFrom({ ...form, [name]: value });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">이름:</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        value={form.name}
+        onChange={handleChange}
+      />
+      <label htmlFor="email">이메일:</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        value={form.email}
+        onChange={handleChange}
+      />
+      <button>Submit</button>
+    </form>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import AppMentors from "./AppMentors";
+import AppFrom from "./AppForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppMentors />
+    <AppFrom />
   </React.StrictMode>
 );
