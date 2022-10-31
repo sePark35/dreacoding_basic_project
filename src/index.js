32610258a123611eb7ad8d22ce1f4c4dc6a3e3c6
gomgom35b <gongwon35.b@gmail.com>
2022-10-31 17:41:36 +0900

    yarn 재설치

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import AppProducts from ".basic/AppProducts";
+import AppProducts from "./basic/AppProducts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
